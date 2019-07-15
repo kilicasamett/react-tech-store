dce81f2a590483c2f03e4c6760eed279d497a289
Abdulsamed KILIÇ <kilicasamed06@gmail.com>
2019-07-15 17:23:32 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+
+function App() {
+  return (
+<h1>Hello from tech store</h1>
+  );
+}
+
+export default App;
